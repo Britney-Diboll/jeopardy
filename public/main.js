686b2b86ff3b05d39af885d03e1119289e13358f
Diboll <bdiboll@proforma.com>
2018-03-31 18:52:03 -0400

    added angular starting point

--- public/main.js
+++ public/main.js
@@ -1,11 +1,13 @@
 
-const api= "http://jservice.io/api/random?count=100"
+const API = "http://jservice.io/api/random?count=100"
 
 
 angular 
   .module("jeopardyBoard", [])
-  .controller("mainController", ($scope) =>{
+  .controller("mainController", ['$scope', '$http', ($scope, $http) =>{
 
-  
-    
-  }
+    $http({
+      method: "GET",
+      url: API,
+    })
+  }]
